be6a201462c41c640109938f4afa5d1d6c2edee7
André Torres <andretorresengm@hotmail.com>
2024-08-15 16:59:24 +0100

    primeiro codigo ruby

---
+++ desafioruby.rb
@@ -0,0 +1,8 @@
+print "digite seu nome: "
+nome = gets.chomp
+print "digite seu sobrenome: "
+snome = gets.chomp
+print "digite sua idade :"
+date = gets.chomp
+
+print "Olá #{nome} #{snome}!, seja benvindo, sua idade é #{date}."
